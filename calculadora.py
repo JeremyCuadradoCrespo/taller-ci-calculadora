e371c58dad6c88554fcc0a6b590179cf930d3077
JeremyCuadradoCrespo <jcuadradoc@unemi.edu.ec>
2026-06-17 22:17:06 -0500

    fix: modificación experimental en la función suma

--- calculadora.py
+++ calculadora.py
@@ -1,14 +1,12 @@
 """
-Módulo de calculadora básica para demostrar flujos de CI.
+Pruebas unitarias para el módulo calculadora.
 """
+from calculadora import suma, resta
 
-def suma(a, b):
-    """Retorna la suma de dos números."""
-    return a + b
+def test_suma():
+    """Valida que la suma funcione correctamente (Forzando fallo)."""
+    assert suma(5, 3) == 99  # <--- Cambia el 8 por 99. Como 5+3 es 8, esto va a fallar.
 
-def resta(a, b):
-    """Retorna la resta de dos números."""
-    return a - b
-
-if __name__ == "__main__":
-    print(f"Suma de 5 + 3 = {suma(5, 3)}")
+def test_resta():
+    """Valida que la resta funcione correctamente."""
+    assert resta(10, 4) == 6
