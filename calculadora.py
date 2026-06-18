7b0a9917c301c611079dc602fb5470e5585004c7
diegowar2525 <diegowar2525@gmail.com>
2026-06-17 22:13:48 -0500

    fix: modificación experimental en la función suma

--- calculadora.py
+++ calculadora.py
@@ -3,12 +3,9 @@ Módulo de calculadora básica para demostrar flujos de CI.
 """
 
 def suma(a, b):
-    """Retorna la suma de dos números."""
     return a + b
 
 def resta(a, b):
-    """Retorna la resta de dos números."""
     return a - b
 
-if __name__ == "__main__":
-    print(f"Suma de 5 + 3 = {suma(5, 3)}")
+print(suma(5, 3))
